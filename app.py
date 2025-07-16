fd761df2946969e67b2527e441f0091cca28310f
Yapypalo <zubko.9797@mail.ru>
2025-07-16 17:36:10 +0900

    Добавил .gitignore, добавил токен для бота, Бот рабочий

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import logging
 import os
+from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+
+load_dotenv()
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
